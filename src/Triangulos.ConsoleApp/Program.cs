80dabd7e8fd397a09115f8ff80ca374f8cbbfbb8
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-04-09 18:19:59 -0300

    Implementando aplicação

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Triangulo triangulo1 = new Triangulo();
+
+            triangulo1.LadoX = 10;
+            triangulo1.LadoY = 10;
+            triangulo1.LadoZ = 10;
+
+            Console.WriteLine(triangulo1.ObterTipo()); // Equilátero
+
+            Triangulo triangulo2 = new Triangulo();
+
+            triangulo2.LadoX = 12;
+            triangulo2.LadoY = 10;
+            triangulo2.LadoZ = 10;
+
+            Console.WriteLine(triangulo2.ObterTipo()); // Isósceles
+
+            Triangulo triangulo3 = new Triangulo();
+
+            triangulo3.LadoX = 12;
+            triangulo3.LadoY = 14;
+            triangulo3.LadoZ = 16;
+
+            Console.WriteLine(triangulo3.ObterTipo()); // Escaleno
+
+            Console.ReadLine();
         }
     }
 }
---
+++ src/Triangulos.ConsoleApp/Triangulo.cs
@@ -0,0 +1,49 @@
+﻿namespace Triangulos.ConsoleApp
+{
+    public class Triangulo
+    {
+        public int LadoX;
+        public int LadoY;
+        public int LadoZ;
+
+        public string ObterTipo()
+        {
+            string tipo = "";
+
+            if (LadoXInvalido() || LadoYInvalido() || LadoZInvalido())
+                tipo = "Triângulo Inválido";
+
+            else if (LadoX == LadoY && LadoY == LadoZ)
+                tipo = "Equilátero";
+
+            else if (LadoX != LadoY && LadoY != LadoZ && LadoZ != LadoX)
+                tipo = "Escaleno";
+
+            else
+                tipo = "Isósceles";
+
+            return tipo;
+        }
+
+        public bool LadoXInvalido()
+        {
+            bool ladoXInvalido = LadoX > LadoY + LadoZ;
+
+            return ladoXInvalido;
+        }
+
+        public bool LadoYInvalido()
+        {
+            bool ladoYInvalido = LadoY > LadoX + LadoZ;
+
+            return ladoYInvalido;
+        }
+
+        public bool LadoZInvalido()
+        {
+            bool ladoZInvalido = LadoZ > LadoX + LadoY;
+
+            return ladoZInvalido;
+        }
+    }
+}
